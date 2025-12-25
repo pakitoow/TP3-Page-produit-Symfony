b91e45e2ccaacf6e854007243c318930ff200818
pakitoow <gumbalwaterson2020@gmail.com>
2025-12-25 04:13:24 -0500

    TP3 Symfony - Page produit

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Controller;
+
+use App\Form\CartItemType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/product', name: 'app_product_show')]
+    public function show(Request $request): Response
+    {
+        $form = $this->createForm(CartItemType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            // Ici, normalement: ajout au panier, redirection, etc.
+            // return $this->redirectToRoute('app_cart_index');
+        }
+
+        return $this->render('product/show.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
